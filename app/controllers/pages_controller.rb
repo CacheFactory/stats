f64ac27116de4f9f5b20b3926f57e4362bd7c87c
Edward Anderson <ejanderson4@gmail.com>
2014-05-05 10:05:20 -0700

    adding back report routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
-  caches_page :index, :expires_in => 5.minutes
+  caches_page :index, :top_urls ,:top_referrers, :expires_in => 5.minutes
   def index
     @data_hash = {top_urls: ReportMaker.top_urls,
                   top_referrers: ReportMaker.top_referrers}
+  end
+
+  def top_urls
+    render json: ReportMaker.top_urls
+  end
+
+  def top_referrers
+    render json: ReportMaker.top_referrers
   end
 end
